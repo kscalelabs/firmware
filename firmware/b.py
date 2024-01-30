97c08067be918f4ff6ea39d403b112e8cc0b4cf5
Benjamin Bolte <ben@bolte.cc>
2024-01-30 14:52:48 -0800

    stuff

--- firmware/b.py
+++ firmware/b.py
@@ -2,6 +2,14 @@
 
 import can
 
-bus = can.interface.Bus(interface="socketcan", channel="can0")
-for _ in range(0, 1000):
-    bus.send(can.Message(arbitration_id=140, data=[0x9c, 0, 0, 0, 0, 0, 0, 0]))
+with can.interface.Bus(interface="socketcan", channel="can0") as bus:
+    for i in range(0, 1000):
+        bus.send(
+            can.Message(
+                channel=i,
+                is_extended_id=False,
+                is_rx=False,
+                data=[0x9C, 0, 0, 0, 0, 0, 0, 0],
+            )
+        )
+        bus.flush_tx_buffer()
