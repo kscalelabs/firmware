2469c1e1e58f4c7f647a1e176c566e83253941cf
Benjamin Bolte <ben@bolte.cc>
2024-03-01 01:16:13 -0500

    add imu library code initial version

--- firmware/cpp/imu/imu.h
+++ firmware/cpp/imu/imu.h
@@ -1,7 +1,11 @@
 #pragma once
 
-#include <iostream>
-
+#include "LIS3MDL.h"
+#include "LSM6DSL.h"
+#include "LSM9DS0.h"
+#include "LSM9DS1.h"
+#include "i2c-dev.h"
 #include <pybind11/pybind11.h>
+#include <stdint.h>
 
 using namespace pybind11::literals;
