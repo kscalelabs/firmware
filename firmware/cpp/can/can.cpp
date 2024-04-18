1066e78ee832382c03b2937adb6a2da212a54bc3
Ben Bolte <ben@bolte.cc>
2024-04-18 15:11:53 -0700

    can driver temp commit (#1)
    * can driver temp commit
    
    * app
    
    * fix lint
    
    * lint
    
    * wip
    
    * asdf
    
    * refactoringg
    
    * api

--- firmware/cpp/can/can.cpp
+++ firmware/cpp/can/can.cpp
@@ -1,71 +1,71 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
-#include <linux/spi/spidev.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "can.h"
 
-int main() {
-    int spi_fd = open("/dev/spidev0.0", O_RDWR);
+MCP_CAN::MCP_CAN(const std::string &device, uint8_t spiMode,
+                 uint8_t bitsPerWord, uint32_t speed)
+    : spiMode(spiMode), bitsPerWord(bitsPerWord), speed(speed) {
+  spi_fd = open(device.c_str(), O_RDWR);
+  if (spi_fd < 0)
+    throw std::runtime_error("Can't open device: " + std::string(device));
 
-    if (spi_fd < 0) {
-        perror("SPI Device open failed");
-        return EXIT_FAILURE;
-    }
+  // Configure SPI.
+  if (ioctl(spi_fd, SPI_IOC_WR_MODE, &spiMode) < 0)
+    throw std::runtime_error("Can't set SPI mode");
+  if (ioctl(spi_fd, SPI_IOC_WR_BITS_PER_WORD, &bitsPerWord) < 0)
+    throw std::runtime_error("Can't set bits per word");
+  if (ioctl(spi_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed) < 0)
+    throw std::runtime_error("Can't set speed");
+}
 
-    // SPI parameters
-    uint8_t spi_mode = SPI_MODE_0;
-    uint8_t bits_per_word = 8;
-    uint32_t speed = 500000; // 500 kHz
+MCP_CAN::~MCP_CAN() {
+  if (close(spi_fd) < 0)
+    throw std::runtime_error("Can't close device");
+}
 
-    // Configure SPI
-    if (ioctl(spi_fd, SPI_IOC_WR_MODE, &spi_mode) < 0) {
-        perror("SPI Mode Change failure");
-        close(spi_fd);
-        return EXIT_FAILURE;
-    }
+void MCP_CAN::transfer(uint8_t *tx, uint8_t *rx, size_t len) {
+  struct spi_ioc_transfer tr = {
+      .tx_buf = (unsigned long)tx,
+      .rx_buf = (unsigned long)rx,
+      .len = len,
+      .speed_hz = speed,
+      .delay_usecs = 0,
+      .bits_per_word = bitsPerWord,
+      .cs_change = 0,
+      .pad = 0,
+  };
 
-    if (ioctl(spi_fd, SPI_IOC_WR_BITS_PER_WORD, &bits_per_word) < 0) {
-        perror("SPI BPW Change failure");
-        close(spi_fd);
-        return EXIT_FAILURE;
-    }
+  if (ioctl(spi_fd, SPI_IOC_MESSAGE(1), &tr) < 0)
+    throw std::runtime_error("Can't transfer data");
+}
 
-    if (ioctl(spi_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed) < 0) {
-        perror("SPI Speed Change failure");
-        close(spi_fd);
-        return EXIT_FAILURE;
-    }
+void MCP_CAN::reset(void) {
+  uint8_t tx[] = {MCP_RESET};
+  uint8_t rx[sizeof(tx)];
+  transfer(tx, rx, sizeof(tx));
+}
 
-    // Prepare data to send (example)
-    uint8_t tx_buffer[] = {0x01, 0x02, 0x03}; // Example data
-    uint8_t rx_buffer[sizeof(tx_buffer)] = {0}; // Receive buffer
+uint8_t MCP_CAN::readRegister(uint8_t address) {
+  uint8_t tx[] = {MCP_READ, address};
+  uint8_t rx[sizeof(tx)];
+  transfer(tx, rx, sizeof(tx));
+  return rx[1];
+}
 
-    struct spi_ioc_transfer spi_transfer = {
-        .tx_buf = (unsigned long)tx_buffer,
-        .rx_buf = (unsigned long)rx_buffer,
-        .len = sizeof(tx_buffer),
-        .speed_hz = speed,
-        .bits_per_word = bits_per_word,
-    };
+void MCP_CAN::setRegister(uint8_t address, uint8_t value) {
+  uint8_t tx[] = {MCP_WRITE, address, value};
+  uint8_t rx[sizeof(tx)];
+  transfer(tx, rx, sizeof(tx));
+}
 
-    // Transmit and receive
-    if (ioctl(spi_fd, SPI_IOC_MESSAGE(1), &spi_transfer) < 0) {
-        perror("SPI Transaction failure");
-        close(spi_fd);
-        return EXIT_FAILURE;
-    }
+namespace py = pybind11;
 
-    printf("Received: ");
-    for (int i = 0; i < sizeof(tx_buffer); i++) {
-        printf("%02X ", rx_buffer[i]);
-    }
-    printf("\n");
+using namespace pybind11::literals;
 
-    // Close SPI device
-    close(spi_fd);
+PYBIND11_MODULE(can, m) {
+  m.doc() = "CAN module for interfacing with MCP2515 CAN controller";
 
-    return 0;
+  py::class_<MCP_CAN>(m, "MCP_CAN")
+      .def(py::init<const char *>(), "device"_a)
+      .def("reset", &MCP_CAN::reset)
+      .def("read_register", &MCP_CAN::readRegister, "address"_a)
+      .def("set_register", &MCP_CAN::setRegister, "address"_a, "value"_a);
 }
