7cffbb1f295ed69c4c0b46345efe7f404b85c234
WT-MM <wesley.maa@gmail.com>
2024-07-10 16:41:31 -0700

    create live plot for imu data

--- firmware/scripts/show_imu.py
+++ firmware/scripts/show_imu.py
@@ -4,6 +4,7 @@
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 from firmware.cpp.imu.imu import IMU, KalmanFilter
 
@@ -13,11 +14,17 @@ def main() -> None:
     parser.add_argument("--dt", type=float, default=0.02, help="The time step between measurements")
     parser.add_argument("--bus", type=int, default=1, help="The I2C bus number")
     parser.add_argument("--raw", default=False, action="store_true", help="Print raw values")
-    parser.add_argument("--print", type=float, default=0.2, help="How often to print readings")    
+    parser.add_argument("--delay", type=float, default=0.2, help="How often to print readings")
+    parser.add_argument("--plot", type=bool, default=True, help="Display a live plot of the readings")
+    parser.add_argument("--print", type=bool, default=True, help="Print out readings")
     args = parser.parse_args()
 
     imu = IMU(args.bus)
     kf = KalmanFilter(imu, min_dt=args.dt)
+    if args.plot:
+        live_plot(args, imu, kf)
+    elif args.print:
+        console(args, imu, kf)
 
     
 def console(args, imu, kf):
@@ -26,7 +33,7 @@ def console(args, imu, kf):
     while True:
         angle = kf.step()
 
-        if printTime > args.print:
+        if printTime > args.delay:
             #print(imu.acc_angle() if args.raw else angle)
             #print(imu.gyr_rate())
             print(imu.get_6DOF())
@@ -34,35 +41,46 @@ def console(args, imu, kf):
         printTime += args.dt
 
 def live_plot(args, imu, kf):
-    def plotter(axs, x_vec, y_data, lines, pause_time=0.1):
-        if lines:
-            for line, data in zip(lines, y_data):
-                line.set_ydata(data)
-                line.set_xdata(x_vec)
-                line.axes.relim()
-                line.axes.autoscale_view()
-        else:
-            lines = [ax.plot(x_vec, data, 'r')[0] for ax, data in zip(axs, y_data)]
-        plt.pause(pause_time)
-        return lines
+    def plotter(axs, lines, new_data, time):
+        for ax, line, data in zip(axs.flat, lines, new_data):
+            x_data, y_data = line.get_xdata(), line.get_ydata()
+            line.set_xdata(np.append(x_data, time))
+            line.set_ydata(np.append(y_data, data))
+            ax.relim()
+            ax.autoscale_view(True, True, True)
+        plt.pause(0.001)
+
+
     # Setup live plotting
-    fig, axs = plt.subplots(3, 2)  # 3 angles and 3 angular velocities
+    fig, axs = plt.subplots(2, 3)  # 3 angles and 3 angular velocities
     plt.ion()
     fig.show()
     fig.canvas.draw()
+    labels = ['Angle 1', 'Angle 2', 'Angle 3', 'Angular Velocity 1', 'Angular Velocity 2', 'Angular Velocity 3']
 
-    size = 100  # Number of points for moving window
-    x_vec = np.linspace(0, 1, size)  # Time vector
-    y_vecs = [np.zeros(size) for _ in range(6)]  # Initialize y data
-    lines = []
+    lines = [ax.plot([], [])[0] for ax in axs.flat]
+    for ax, label in zip(axs.flat, labels):
+        ax.set_title(label)
+        ax.set_xlabel('Time (s)')
+        ax.set_ylabel('Degrees' if 'Angle' in label else 'Degrees/s')
 
+    last = time.time()
     while True:
-        angle = kf.step()
-        dof6 = imu.get_6DOF()  # Expected to return a list of 6 values
-        data = [dof6.yaw, dof6.pitch, dof6.roll, dof6.x, dof6.y, dof6.z]
+        current = time.time()
 
-        y_vecs = [np.append(y_vec[1:], new) for y_vec, new in zip(y_vecs, data)]
-        lines = plotter(axs.flat, x_vec, y_vecs, lines)
+        angle = kf.step()
+
+        elapsed = current - last
+
+        dof6 = imu.get_6DOF()  # Expected to return a list of 6 values
+        data = [angle.yaw, angle.pitch, angle.roll, dof6.x, dof6.y, dof6.z]
+
+        if args.print:
+            print(dict(zip(["Yaw", "Pitch", "Roll", "x", "y", "z"], data)))
+
+        plotter(axs, lines, data, time.time())
+        last = current
+
 
 
 if __name__ == "__main__":
