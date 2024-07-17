6cb99aaf252a2fb30967cb99c743687993d98bde
WT-MM <wesley.maa@gmail.com>
2024-07-17 13:13:57 -0700

    new current calculation

--- firmware/scripts/move_legs.py
+++ firmware/scripts/move_legs.py
@@ -29,18 +29,18 @@ from firmware.robot.robot import Robot
 
 
 # Utility functions
-def degrees_to_int(degrees: float) -> int:
-    return max(0, min())
+def degrees_to_radians(degrees: float) -> float:
+    return degrees * (3.14159 / 180)
 
 def test_motor(robot: Robot, config: Dict, motor_num: int) -> None:
     robot.test_motor(config["motors"][motor_num], sign=config["signs"][motor_num])
 
 
 def test_torque_control(robot: Robot, config: Dict) -> None:
-
+    # In an ideal world, kt would actually be the torque constant, but we're using it as a scaling factor
     def calculate_motor_current(pos_desired: int, pos_current: int,
                                 speed_desired: int, speed_current: int,
-                                torque_ff, kp=75, kd=3, kt=1) -> int:
+                                torque_ff, kp=15, kd=0.5, kt=25) -> int:
         control_effort = kp * (pos_desired - pos_current) + kd * (speed_desired - speed_current) + torque_ff / kt
         return control_effort
     
@@ -73,5 +73,25 @@ def main() -> None:
     # config = robot.motor_config["right_leg"]
     #test_motor(robot, config, 0)
 
+def mac():
+    def calculate_motor_current(pos_desired: int, pos_current: int,
+                                speed_desired: int, speed_current: int,
+                                torque_ff, kp=15, kd=0.5, kt=25) -> int:
+        control_effort = (kp * (pos_desired - pos_current) + kd * (speed_desired - speed_current) + torque_ff) / kt
+        return control_effort
+
+    test_position = 60
+    pos_current = 0
+    while True:
+        speed_current = 0
+        torque_ff = 0
+        control_effort = calculate_motor_current(test_position, pos_current, 0, speed_current, torque_ff)
+        print(f"Motor error: {pos_current - test_position} control effort: {control_effort}")
+        pos_current += 1
+        if pos_current > 60:
+            pos_current = 0
+        time.sleep(0.4)
+
 if __name__ == "__main__":
+    #mac()
     main()
