dc180d5c4a0ab5d96c589334b30d2751de6c6354
WT-MM <wesley.maa@gmail.com>
2024-08-25 14:00:24 -0700

    add imu log

---
+++ firmware/scripts/test_walking/log_imu_data.py
@@ -0,0 +1,28 @@
+"""Script to log imu data from robot. Writes to out.csv file."""
+
+import math
+import time
+
+from firmware.imu.imu import IMUInterface
+
+
+def main() -> None:
+    """Log imu data from robot."""
+    imu = IMUInterface(1)
+    imu_dt = time.time()
+    imu_readings = []
+    for _ in range(2000):
+        imu_data = imu.step(time.time() - imu_dt)
+        imu_dt = time.time()
+        eu_ang = imu_data[0]
+        eu_ang[eu_ang > math.pi] -= 2 * math.pi
+        imu_readings.append(eu_ang)
+        print(eu_ang)
+        time.sleep(0.01)
+    with open("out.csv", "w") as f:
+        for reading in imu_readings:
+            f.write(f"{reading[0]},{reading[1]},{reading[2]}\n")
+
+
+if __name__ == "__main__":
+    main()
