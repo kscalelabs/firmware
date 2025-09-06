e244edd2f1bb6a7f65dd896aa45b8a9a5bc864a1
Eric Pedley <ericpedley@gmail.com>
2025-09-05 22:54:07 -0700

    change gripper mapping based on zeroing at open position

--- src/inference.rs
+++ src/inference.rs
@@ -655,12 +655,9 @@ impl std::fmt::Debug for Operate {
 
 fn gripper_position_to_joint_angle(pos: f64) -> f64 {
     // clamp pos from 0 to 68
-    let clamped_pos_mm = pos.clamp(0.0, 0.068)*1e3;
+    let clamped_pos_mm = pos.clamp(0.0, 0.068);
     // quadratic ax^2+bx+c
-    let a = 0.0000374545;
-    let b = -0.0170176;
-    let c = 0.61298;
-    let angle = a * clamped_pos_mm * clamped_pos_mm + b * clamped_pos_mm + c;
+    let angle = 1-clamped_pos_mm / 0.068;
     info!("Mapped gripper position {pos} to joint angle {angle}");
     angle
 }
