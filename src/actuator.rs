616ab60213761e3959df4b181baf3741e04e2ed0
Eric Pedley <ericpedley@gmail.com>
2025-09-05 20:02:16 -0700

    increase n to get around asusmption that actuator ids are sequential

--- src/actuator.rs
+++ src/actuator.rs
@@ -718,7 +718,7 @@ async fn read_responses_update(
         return Err(std::io::Error::other("Socket is not in Operate state"));
     };
 
-    let n = ss.actuator_clients.len();
+    let n = 8; // TODO: make this dynamic but also not reliant on the actuator ids being sequential
     
     // Pre-compute servo ID mapping to avoid borrow conflicts
     let actuator_ids: Vec<u8> = ss.actuator_clients.iter()
