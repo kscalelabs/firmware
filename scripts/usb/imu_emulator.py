6db873e369d4c326008b30193d0be7c7c745c081
Eric Pedley <ericpedley@gmail.com>
2025-09-09 15:26:24 -0700

    add printing to see if progress

--- scripts/usb/imu_emulator.py
+++ scripts/usb/imu_emulator.py
@@ -69,11 +69,18 @@ def main():
 
     print(f"Writing Hiwonder quaternion frames to {args.port} @ {args.rate}Hz (interval {interval}s)")
 
+    spinner = ['|', '/', '-', '\\']
+    spinner_index = 0
+
     try:
         while True:
             frame = make_quat_frame(w_raw, x_raw, y_raw, z_raw)
             try:
                 os.write(fd, frame)
+                # print loading spinner
+                print(f"\r{spinner_index} Sent frame: {frame.hex()}", end='', flush=True)
+                spinner_index+=1
+
             except BrokenPipeError:
                 print("Peer closed PTY (broken pipe). Exiting.", file=sys.stderr)
                 break
