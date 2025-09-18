f44fe70d65cff2159523b2f5bd3d61364c4ad6f4
Eric Pedley <ericpedley@gmail.com>
2025-09-17 19:15:03 -0700

    fix wrong metho dname

--- src/udp_command.rs
+++ src/udp_command.rs
@@ -33,7 +33,7 @@ impl UnifiedUdpManager {
             socket,
             current_command: UdpExtendedCommand::default(),
             last_command_time: None,
-            command_timeout: Duration::from_seconds(30),
+            command_timeout: Duration::from_secs(30),
         })
     }
 
